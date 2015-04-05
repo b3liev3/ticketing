215bfca1e229fdd3ed5ed6ff3d20765a4f62ab86
b3liev3 <pere@soms.cat>
2015-04-05 18:11:09 +0200

    Firs commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+/* App Module*/
+
+var blogApp = angular.module('blogApp', [
+    'ngRoute',
+    'blogControllers',
+    'blogDirectives'
+]);
+
+blogApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        $routeProvider.
+                when('/', {
+                    templateUrl: 'partials/main.html',
+                    controller: 'BlogCtrl'
+                }).
+                when('/blogPost/:id', {
+                    templateUrl: 'partials/blogPost.html',
+                    controller: 'BlogViewCtrl'
+                });
+    }]);
